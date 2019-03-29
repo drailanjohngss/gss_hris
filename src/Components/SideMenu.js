65b6e83250e0f76fd41167fbdab9b892d69c09df
Drailan John Terrible <drailanjohn.gss@gmail.com>
2019-03-29 23:21:17 +0800

    added icons

--- src/Components/SideMenu.js
+++ src/Components/SideMenu.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Box, Anchor } from 'grommet';
+import { Box, Anchor, Text } from 'grommet';
 import styled from 'styled-components';
 import Container from '../Components/Container';
+import { Calendar } from 'grommet-icons';
 
 class SideMenu extends Component {
   render() {
@@ -17,26 +18,35 @@ class SideMenu extends Component {
       width: 100%;
       list-style-type: none;
       display: flex;
-      justify-content: center;
-      padding: 20px 0;
+      justify-content: start;
+      padding: 20px 0 20px 10px;
       border-bottom: white 2px solid;
     `;
     return (
-      <Container basis="20%" height="100vh" background="accent-3">
+      <Container basis="20%" height="100vh">
         <MenuWrapper>
           <MenuList>
-            <Anchor color="accent-5" size="small">
-              Attendance
+            <Anchor color="accent-5" size="large">
+              <Box alignSelf="center" direction="row">
+                <Calendar color="brand" />
+                <Text margin={{ left: '20px' }}> Attendance </Text>
+              </Box>
             </Anchor>
           </MenuList>
           <MenuList>
-            <Anchor color="accent-5" size="small">
-              Leaves
+            <Anchor color="accent-5" size="large">
+              <Box alignSelf="center" direction="row">
+                <Calendar color="brand" />
+                <Text margin={{ left: '20px' }}> Leaves </Text>
+              </Box>
             </Anchor>
           </MenuList>
           <MenuList>
-            <Anchor color="accent-5" size="small">
-              Early Out
+            <Anchor color="accent-5" size="large">
+              <Box alignSelf="center" direction="row">
+                <Calendar color="brand" />
+                <Text margin={{ left: '20px' }}> Early Out </Text>
+              </Box>
             </Anchor>
           </MenuList>
         </MenuWrapper>
